729cbf4d748afcea3a0bd50f9277bcdf8d82964a
danchetto <danchetto@yandex-team.ru>
2019-02-11 00:03:14 +0300

    + title

---
+++ src/ui/Title/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { block } from 'bem-cn';
+
+import './index.scss';
+
+interface TitleProps {
+    text?: string;
+    size?: 'small' | 'medium' | 'large';
+}
+
+const b = block('ui-title');
+
+export default class Title extends React.Component<TitleProps> {
+    public render() {
+        const { text, size } = this.props;
+
+        return (
+            <h1 className={b({ size })}> {text} </h1>
+        );
+    }
+}
