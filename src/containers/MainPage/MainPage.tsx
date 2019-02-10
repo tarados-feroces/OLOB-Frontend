96884da7074ba114784139554010384cb6171101
danchetto <danchetto@yandex-team.ru>
2019-02-11 01:27:34 +0300

    убрал из mainPage

--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 
 import './MainPage.scss';
-import List from '../../ui/List';
 
 export default class MainPage extends React.Component {
     public render(): JSX.Element {
-        return (
-            <List data={[ 'first', 'second', 'cool' ]} />
-        );
+        return ;
     }
 }
